1cb3f6adc6e6e4ba86a7c976bb9213fb4e7fd619
Ninad <ninadkalanke@gmail.com>
2021-03-25 15:17:02 +0530

    Added file system

---
+++ Edith_resource.py
@@ -0,0 +1,6 @@
+chromepath = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+codepath = 'C:/Users/91942/AppData/Local/Programs/Microsoft VS Code/Code.exe'
+spotifypath = 'C:/Users/91942/AppData/Roaming/Spotify/Spotify.exe'
+
+email_dictonary_name = ['pankaj', 'niketan', 'jay']
+email_dictonary_email = ['pksmks1999@gmail.com', 'niketanmangulley@gmail.com', 'jayraikhere@gmail.com']
---
+++ Edith_speak.py
@@ -0,0 +1,31 @@
+import pyttsx3
+import speech_recognition as sr
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def takeCommand():
+    # It take microphone input and returns string output
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 0.8
+        audio = r.listen(source)
+        
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User Said: {query}\n")
+
+    except Exception as e:
+        # print(e)
+        speak("Say that again please...")
+        return 'none'
+
+    return query
+
